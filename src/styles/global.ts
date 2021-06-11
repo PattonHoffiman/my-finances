cf23d981b428ff2d5ce85b19a17c7b431b73ef37
Patton Hoffiman <PattonHoffiman@gmail.com>
2021-06-11 00:14:21 -0300

    [.ts] Create Global Styles

---
+++ src/styles/global.ts
@@ -0,0 +1,66 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    // Base Colors
+    --white: #f2f2f2;
+    --dark-blue: #162730;
+    --light-blue: #415463;
+    --darkest-blue: #09141a;
+    --lightest-blue: #afbbc7;
+    --complementary-red: #ff3100;
+    --complementary-green: #05b07b;
+
+    // Glass Effect
+    --blur: 5px;
+    --shadow: rgba(0,0,0,.5);
+    --glass: rgba(255,255,255,.1);
+    --glass-border: rgba(255,255,255,.5);
+  }
+  
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: var(--darkest-blue);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-weight: 400;
+    font-family: 'Roboto Slab', serif;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: .6;
+    cursor: not-allowed;
+  }
+
+  .glass {
+    background: var(--glass);
+    backdrop-filter: blur(5px);
+    -webkit-backdrop-filter: blur(5px);
+    box-shadow: 4px 4px 4px var(--shadow);
+  }
+`;

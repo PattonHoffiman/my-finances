57e5949edc56f27b86df98b498dc345370112efc
Patton Hoffiman <PattonHoffiman@gmail.com>
2021-06-11 00:14:55 -0300

    [.tsx] Add Global Styles to App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { GlobalStyle } from './styles/global';
+
 export function App() {
   return (
-    <div className="App">
-      <h1>Hello First App!</h1>
-    </div>
+    <>
+      < GlobalStyle />
+      <div>
+        <h1>Hello First App!</h1>
+      </div>
+    </>
   );
 }

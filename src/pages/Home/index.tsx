b4be93926518c4466c19709304902185853411f3
Patton Hoffiman <PattonHoffiman@gmail.com>
2021-06-17 00:03:44 -0300

    [Pages] Update Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,48 @@
+import { useState } from 'react';
 import { Main } from "../../components/Main";
 import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
 import { FloatButton } from "../../components/FloatButton";
 
 import { Summary } from "../../containers/Summary";
+import { Transactions } from "../../containers/Transactions";
 
-export const Home: React.FC = () => (
-  <>
-    <Header />
-    <Main>
-      <Summary />
-      <FloatButton />
-    </Main>
-  </>
-);
+export const Home: React.FC = () => {
+  const [showSummary, setShowSummary] = useState(true);
+  const [buttonAnimate, setButtonAnimate] = useState('menu');
+  const [showTransactions, setShowTransactions] = useState(false);
+
+  function animateSummary(): void {
+    setButtonAnimate('menu');
+    
+    setShowTransactions(false);
+    setShowSummary(true);
+  }
+
+  function animateTransitions(): void {
+    setButtonAnimate('transactions');
+
+    setShowSummary(false);
+    setShowTransactions(true);
+  }
+
+  return (
+    <>
+      <Header />
+      <Main>
+        <Section>
+          <Summary show={showSummary} />
+          <Transactions
+            show={showTransactions}
+            animateSummary={animateSummary}
+          />
+        </Section>
+        <FloatButton
+          buttonAnimate={buttonAnimate}
+          animateSummary={animateSummary}
+          animateTransitions={animateTransitions}
+        />
+      </Main>
+    </>
+  );
+}

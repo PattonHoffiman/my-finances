91f2c15546e90b1c1be6eefe923d8982ebf22033
Patton Hoffiman <PattonHoffiman@gmail.com>
2021-06-17 00:02:28 -0300

    [Components] Update Summary

--- src/containers/Summary/index.tsx
+++ src/containers/Summary/index.tsx
@@ -1,29 +1,40 @@
-import { Section } from '../../components/Section';
+import { useSpring, animated as a } from 'react-spring';
+
+import { Info } from '../../components/Info';
+import { Content } from '../../components/Content';
 import { InnerHeader } from '../../components/InnerHeader';
+import { HeaderContent, ContentValue, ContentTitle } from './style';
 
-import { HeaderContent, Content, Info } from './style';
+interface ISummaryProps {
+  show: boolean;
+}
 
-export function Summary() {
-  return (
-    <Section>
+export const Summary: React.FC<ISummaryProps> = ({ show }) => {
+  const props = useSpring({
+    opacity: show ? 1 : 0,
+    display: show ? 'block' : 'none',
+  });
+
+  return (    
+    <a.div style={props}>
       <InnerHeader>
         <HeaderContent>June</HeaderContent>
         <HeaderContent className="light">2021</HeaderContent>
       </InnerHeader>
       <Content>
         <Info>
-          <strong>Incomes</strong>
-          <span>R$ 5.000,00</span>
+          <ContentTitle>Incomes</ContentTitle>
+          <ContentValue>R$ 5.000,00</ContentValue>
         </Info>
         <Info>
-          <strong>Outcomes</strong>
-          <span>R$ 3.245,00</span>
+          <ContentTitle>Outcomes</ContentTitle>
+          <ContentValue>R$ 3.245,00</ContentValue>
         </Info>
         <Info className="highlight-green">
-          <strong>Total</strong>
-          <span>R$ 2.755,00</span>
+          <ContentTitle className="highlight">Total</ContentTitle>
+          <ContentValue>R$ 2.755,00</ContentValue>
         </Info>
       </Content>
-    </Section>
+    </a.div>
   );
 }
--- src/containers/Summary/style.ts
+++ src/containers/Summary/style.ts
@@ -1,54 +1,28 @@
 import styled from 'styled-components';
 
-export const HeaderContent = styled.span`  
+export const ContentValue = styled.span`
+  font-weight: 600;
+  font-size: 1.5rem;
+  margin-bottom: .5rem;
+  font-family: 'Orbitron', monospace;
+`;
+
+export const HeaderContent = styled.span`
   font-weight: 600;
   margin-top: .5rem;
-  font-size: 2.5rem;
+  font-size: 2.5rem;  
 
   &.light {
     color: var(--white);
   }
 `;
 
-export const Content = styled.div`
-  display: grid;
-  margin: .5rem 1.5rem;
-  padding-bottom: 1rem;
-  grid-template-rows: repeat(3, 1fr);
-`;
+export const ContentTitle = styled.strong`
+  font-weight: 400;
+  margin-top: .5rem;
+  font-size: 1.25rem;
 
-export const Info = styled.div`
-  display: flex;
-  flex-direction: column;
-
-  width: 100%;
-  height: 75px;
-  margin-bottom: .5rem;
-  border-radius: .5rem;
-  background: var(--white);
-  font-family: 'Orbitron', monospace;
-  
-  strong {
-    font-weight: 400;
-    margin-top: 1rem;
-    margin-left: 1rem;
-    font-size: 1.25rem;
-  }
-
-  span {
-    font-weight: 600;
-    font-size: 1.5rem;
-    margin-left: 1rem;
-    margin-top: .25rem;
-  }
-
-  &.highlight-red {
-    color: var(--white);
-    background: var(--complementary-red);
-  }
-
-  &.highlight-green {
-    color: var(--white);
-    background: var(--complementary-green);
+  &.highlight {
+    font-weight: 500;
   }
 `;

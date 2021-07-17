8b620d9a348aeb5fe6d70a8ebad13348bd278634
Patton Hoffiman <pattonhoffiman@gmail.com>
2021-07-17 17:32:09 -0300

    [.ts] Add Mirage Create Method

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { create } from './services/mirage';
 
 import { App } from './App';
+
+create();
 
 ReactDOM.render(
   <React.StrictMode>
